e15a1892616d9ce97ffd3054fbcd522d5a3ad370
Liv44 <moreauolivia49@gmail.com>
2022-02-07 19:27:52 +0100

    Ajout page people

---
+++ src/components/people/ListPeople.vue
@@ -0,0 +1,17 @@
+<script setup>
+import CardPeople from "./CardPeople.vue";
+import { ref } from "vue";
+defineProps({
+  people: Array,
+});
+</script>
+
+<template>
+  <div class="container w-full p-3 mx-auto flex justify-center">
+    <CardPeople
+      v-for="person in people"
+      :key="person"
+      :person="person"
+    ></CardPeople>
+  </div>
+</template>
