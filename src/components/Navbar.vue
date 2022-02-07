9488b1c5ca350cf46222b5f40c2c1838a66a88d8
Bertyn Boulikou <bboulikou@gmail.com>
2022-02-07 16:18:45 +0100

    add nkavigation page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-link to="/" class="text-xl font-bold mx-2">Home</router-link>
+    <router-link to="/people" class="text-xl font-bold mx-2"
+      >People</router-link
+    >
+    <router-link to="/starship" class="text-xl font-bold mx-2"
+      >Starship</router-link
+    >
+    <router-link to="/vehicle" class="text-xl font-bold mx-2"
+      >Vehicle</router-link
+    >
+    <router-link to="/specie" class="text-xl font-bold mx-2"
+      >Specie</router-link
+    >
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -17,3 +17,5 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+export default router;
