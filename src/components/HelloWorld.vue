19239808adfbd885611054ba3edff185cd451bf0
Bertyn Boulikou <bboulikou@gmail.com>
2022-02-07 14:47:13 +0100

    init du projet

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1 class="text-5xl font-bold">{{ msg }}</h1>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
