19a267389c9ea7f71c3a050e86294c7cf3e1ed34
Fquicy <florien.quicy@ynov.com>
2022-02-08 22:54:03 +0100

    up

--- src/components/specie/CardSpecie.vue
+++ src/components/specie/CardSpecie.vue
@@ -15,7 +15,7 @@ let imgUrl = computed(
 <template>
   <div v-if="spec != null" class="group w-32 h-64 pt-2 m-2 overflow-hidden">
     <img :src="imgUrl" class="w-full h-3/4 transition-all delay-150 hover:-translate-y-1 hover:scale-110" alt=""/>
-      <p class="h-1/4 invisible group-hover:visible text-sm mb-5">{{ spec.name }}</p>
+      <p class="h-1/4 invisible group-hover:visible text-sm mt-5">{{ spec.name }}</p>
       <!--<p class="text-2xl font-bold">{{ spec.classification }}</p>
       <p class="text-2xl font-bold">{{ spec.designation }}</p> -->
   </div>
