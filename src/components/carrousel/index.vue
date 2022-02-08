f24be4bc6531474ca6a8f260d1a5927002dc6cb9
Fquicy <florien.quicy@ynov.com>
2022-02-09 00:00:23 +0100

    up

--- src/components/carrousel/index.vue
+++ src/components/carrousel/index.vue
@@ -5,7 +5,7 @@ const currentSlide = ref(0);
 </script>
 
 <template>
-  <div class="w-full h-96 max-h-96 bg-red-400 relative">
+  <div class="container w-full mt-10 p-3 mx-auto flex justify-center">
     <slot :currentSlide="currentSlide"></slot>
   </div>
 </template>
