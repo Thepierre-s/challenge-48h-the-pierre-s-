4d4356c7bbe6b557fddfb30ba973f09f00875d6e
YasserSeryas <72004928+YasserSeryas@users.noreply.github.com>
2022-02-08 11:26:12 +0100

    Add list Starship

---
+++ src/components/starship/ListStarship.vue
@@ -0,0 +1,20 @@
+<script setup>
+import CardStarship from "./CardStarship.vue";
+import { ref, onMounted } from "vue";
+import Starship from "../../service/module/starship";
+let starships = ref(null);
+onMounted(async () => {
+  let data = await Starship.listStarship();
+  starships.value = data.data.results;
+});
+</script>
+
+<template>
+  <div class="container w-full p-3 mx-auto flex justify-center">
+    <CardStarship
+      v-for="star in starships"
+      :key="star"
+      :starship="star"
+    ></CardStarship>
+  </div>
+</template>
