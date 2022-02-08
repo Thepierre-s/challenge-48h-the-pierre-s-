ee37e5360c166880e4f625784697fd872c8490b0
YasserSeryas <72004928+YasserSeryas@users.noreply.github.com>
2022-02-08 11:09:25 +0100

    AddList Vehicle

--- src/components/vehicle/ListVehicle.vue
+++ src/components/vehicle/ListVehicle.vue
@@ -1,19 +1,22 @@
-
-
 <script setup>
-import Vehicle from "/src/views/CardVehicle.vue";
-import { ref } from "vue";
-defineProps({
-  Vehicle: Array,
+import CardVehicle from "./CardVehicle.vue";
+import { ref, onMounted } from "vue";
+import Vehicle from "../../service/module/vehicle";
+let vehicles = ref(null);
+onMounted(async () => {
+  let data = await Vehicle.listVehicle();
+  vehicles.value = data.data.results;
 });
 </script>
 
 <template>
   <div class="container w-full p-3 mx-auto flex justify-center">
     <CardVehicle
-      v-for="vehicle in Vehicle"
-      :key="vehicle"
-      :vehicle="vehicle"
-    ></CardVehicle>
+  
+      v-for="vehi in vehicles"
+      :key="vehi"
+      :vehicle="vehi"
+    ></CardVehicle
+  >
   </div>
 </template>
