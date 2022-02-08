ed2b9d2ef2f674596173d6c9d0ce71c0aaa9c6d6
Liv44 <moreauolivia49@gmail.com>
2022-02-08 09:52:04 +0100

    add test id people

--- src/components/people/ListPeople.vue
+++ src/components/people/ListPeople.vue
@@ -5,17 +5,23 @@ import { onMounted, ref } from "vue";
 defineProps({
   people: Array,
 });
-// const count = ref(0);
-// const emit = defineEmits(["increment"]);
+const count = ref(0);
+const emit = defineEmits(["increment"]);
+const id = ref(1);
 
-// function Less() {
-//   count.value--;
-//   emit("increment", count.value);
-// }
-// function More() {
-//   count.value++;
-//   emit("increment", count.value);
-// }
+function Less() {
+  count.value--;
+  emit("increment", count.value);
+}
+function More() {
+  count.value++;
+  emit("increment", count.value);
+}
+
+function clickeOnPeople(idEntered) {
+  id.value = idEntered;
+  emit("increment", id.value);
+}
 let pageNumber = ref(null);
 
 pageNumber.value = 1;
@@ -27,13 +33,18 @@ pageNumber.value = 1;
     class="container w-full p-3 mx-auto flex justify-center"
     id="example-1"
   >
-    <button>previous</button>
+    <button @click="Less">previous</button>
     <CardPeople
       v-for="person in people"
       :key="person"
       :person="person"
       :pageNumber="pageNumber"
+      @click="
+        clickeOnPeople(person.url.split('/')[person.url.split('/').length - 2])
+      "
     ></CardPeople>
-    <button>next</button>
+    <button @click="More">next</button>
+    <p>{{ count }}</p>
+    <p>ID du people : {{ id }}</p>
   </div>
 </template>
