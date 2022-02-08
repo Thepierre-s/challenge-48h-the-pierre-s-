d3a63c7c5eec6bee3db4c210049bacb8afd87a9f
Bertyn Boulikou <bboulikou@gmail.com>
2022-02-08 09:22:48 +0100

    fix bug font

--- src/views/Specie.vue
+++ src/views/Specie.vue
@@ -1,38 +1,22 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import {ref, onMounted} from 'vue'
+import { ref, onMounted } from "vue";
 import Specie from "../components/CardSpecie.vue";
 import Spec from "../service/module/specie";
-let OneSpecie=ref(null);
+let OneSpecie = ref(null);
 onMounted(async () => {
-    let i =1;
-  
-let data = await Spec.getSpecieById(i);
+  let i = 1;
+
+  let data = await Spec.getSpecieById(i);
   console.log(data.data);
   OneSpecie.value = data.data;
-  console.log(OneSpecie.value); 
-    
+  console.log(OneSpecie.value);
 });
 </script>
 
 <template>
-
-  <Specie 
-   :spec="OneSpecie"
-  />
-  
-
-  
+  <Specie :spec="OneSpecie" />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/views/Vehicle.vue
+++ src/views/Vehicle.vue
@@ -1,38 +1,22 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import {ref, onMounted} from 'vue'
+import { ref, onMounted } from "vue";
 import Vehicle from "../components/CardVehicle.vue";
 import vehicle from "../service/module/vehicle";
-let OneVehicle=ref(null);
+let OneVehicle = ref(null);
 onMounted(async () => {
-    let i =4;
-  
-let data = await vehicle.getVehicleById(i);
+  let i = 4;
+
+  let data = await vehicle.getVehicleById(i);
   console.log(data.data);
   OneVehicle.value = data.data;
-  console.log(OneVehicle.value); 
-    
+  console.log(OneVehicle.value);
 });
 </script>
 
 <template>
-
-  <Vehicle 
-   :vehicle="OneVehicle"
-  />
-  
-
-  
+  <Vehicle :vehicle="OneVehicle" />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
