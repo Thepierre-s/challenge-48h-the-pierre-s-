66ccea8372f168ec455c654645dc4119a2f6ab2c
Fquicy <florien.quicy@ynov.com>
2022-02-08 08:07:55 +0100

    text translated

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex flex-col justify-center items-center">
        <h1 class="text-9xl font-bold tracking-normal m-20">star<br>wars</h1> 
-       <h2 class="text-5xl m-6"> Bonjour jeune Jedi</h2>
-        <span class="text-xl">Plongez dans l'univers starwars et découverez les secrets de l'empire est rebel</span>
+       <h2 class="text-5xl m-6">Hello young Jedi</h2>
+        <span class="text-xl">Immerse yourself in the star wars universe and discover the secrets of the empire</span>
     </div>
 </template>
 
