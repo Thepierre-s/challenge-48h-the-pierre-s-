5b36f0ade56a4a0e72850543aee51e36fad8f05a
YasserSeryas <72004928+YasserSeryas@users.noreply.github.com>
2022-02-08 11:44:56 +0100

    AddList Film

---
+++ src/components/film/ListFilm.vue
@@ -0,0 +1,20 @@
+<script setup>
+import CardFilm from "./CardFilm.vue";
+import { ref, onMounted } from "vue";
+import Film from "../../service/module/film";
+let films = ref(null);
+onMounted(async () => {
+  let data = await Film.listFilm();
+  films.value = data.data.results;
+});
+</script>
+
+<template>
+  <div class="container w-full p-3 mx-auto flex justify-center">
+    <CardFilm
+      v-for="mov in films"
+      :key="mov"
+      :film="mov"
+    ></CardFilm>
+  </div>
+</template>
