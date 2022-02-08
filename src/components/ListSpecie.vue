44447f132319d2506b25e0be0073dddfe0d791d3
YasserSeryas <72004928+YasserSeryas@users.noreply.github.com>
2022-02-08 08:09:27 +0100

    Update ListSpecie.vue

--- src/components/ListSpecie.vue
+++ src/components/ListSpecie.vue
@@ -1,8 +1,20 @@
-<template>
-    <div>
-        <Specie v-for=""></Specie>
-    </div>
-</template>
+
+
+
 <script setup>
 import Specie from "/src/views/CardSpecie.vue";
+import { ref } from "vue";
+defineProps({
+  Specie: Array,
+});
 </script>
+
+<template>
+  <div class="container w-full p-3 mx-auto flex justify-center">
+    <CardSpecie
+      v-for="spec in Specie"
+      :key="spec"
+      :spec="spec"
+    ></CardSpecie>
+  </div>
+</template>
