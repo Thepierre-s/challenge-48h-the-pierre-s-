8da8b8db8b5ca15f5e69252d6fc5b978ec43a107
Fquicy <florien.quicy@ynov.com>
2022-02-08 08:13:28 +0100

    update

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col justify-center items-center">
        <h1 class="text-9xl font-bold tracking-normal m-20">star<br>wars</h1> 
        <h2 class="text-5xl m-6">Hello young Jedi</h2>
-        <span class="text-xl">Immerse yourself in the star wars universe and discover the secrets of the empire</span>
+        <span class="text-xl">immerse yourself in the star wars universe and discover the secrets of the empire</span>
     </div>
 </template>
 
