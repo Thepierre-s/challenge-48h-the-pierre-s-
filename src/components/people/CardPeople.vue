ed2e92695e11002e825e9dbb963e4d135d44d16b
Liv44 <moreauolivia49@gmail.com>
2022-02-08 08:29:56 +0100

    modif people + add img

--- src/components/people/CardPeople.vue
+++ src/components/people/CardPeople.vue
@@ -1,12 +1,19 @@
 <script setup>
-defineProps({
+import { ref } from "vue";
+const props = defineProps({
   person: Object,
+  pageNumber: Number,
 });
+let urlImg = ref(null);
+urlImg.value =
+  "./src/assets/img/characters/" +
+  props.person.url.split("/")[props.person.url.split("/").length - 2] +
+  ".jpg";
 </script>
 
 <template>
-  <div class="w-32 h-32">
-    <p>Nom : {{ person.name }}</p>
-    <img :src="person.img" />
+  <div class="w-32 h-64 border-2 p-2">
+    <p>{{ person.name }}</p>
+    <img :src="urlImg" />
   </div>
 </template>
