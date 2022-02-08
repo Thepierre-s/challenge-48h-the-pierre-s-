8ce05a08cd3ea697af579479186e9ded5c2c0b95
tetralox <maxime.chevalier@ynov.com>
2022-02-08 09:15:51 +0100

    correction url species dans people.js

--- src/service/module/people.js
+++ src/service/module/people.js
@@ -3,21 +3,21 @@ import ChangeUrl from "../ChangeUrl"
 
 export default {
   async listPeople(page = 1) {
-    let dataGroupes = ["films","homeworld","vehicles","starships"]
+    let dataGroupes = ["films","homeworld","vehicles","starships","species"]
     let data = await apiClient.get("/people/?page=" + page)
     data.data["results"]=await ChangeUrl.multiplesUrlToIdAndName(data.data["results"],dataGroupes)
     return data;
   },
 
   async getPeopleById(id) {
-    let dataGroupes = ["films","homeworld","vehicles","starships"]
+    let dataGroupes = ["films","homeworld","vehicles","starships","species"]
     let data = await apiClient.get("/people/" + id)
     data.data["results"]=await ChangeUrl.urlToIdAndName(data.data,dataGroupes)
     return data;
   },
 
   async searchPeople(searchInput) {
-    let dataGroupes = ["films","homeworld","vehicles","starships"]
+    let dataGroupes = ["films","homeworld","vehicles","starships","species"]
     let data = await apiClient.get("/people/?search=" + searchInput)
     data.data["results"]=await ChangeUrl.urlToIdAndName(data.data["results"][0],dataGroupes)
     return data;
