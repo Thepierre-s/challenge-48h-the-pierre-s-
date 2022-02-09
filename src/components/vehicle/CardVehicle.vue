a8c2dffa91de625d2a461c14a01e35b3a83f25d9
Fquicy <florien.quicy@ynov.com>
2022-02-09 01:37:38 +0100

    up

--- src/components/vehicle/CardVehicle.vue
+++ src/components/vehicle/CardVehicle.vue
@@ -12,7 +12,6 @@ vehicle:Object
 <template>
 <div v-if="vehicle!=null" class=" w-72  h-[500px]  m-2 shadow-md rounded-sm">
         <img :src="vehicle.url_img" class="w-full h-2/3 border-2 border-b-yellow-400" alt="">
-
       <p  class="text-3xl font-bold font-gray-200">{{ vehicle.name }}</p>
       <p class="text-2xl font-bold">{{ vehicle.model }}</p>
       <p class="text-2xl font-bold">{{ vehicle.manufacturer }}</p>
