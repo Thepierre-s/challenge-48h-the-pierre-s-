2037e1486e5c2e5a937dfcee0b42054c29d88ea1
Bertyn Boulikou <bboulikou@gmail.com>
2022-02-09 07:09:56 +0100

    ajout navbar responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,23 +1,100 @@
+<script setup>
+import { ref } from "vue";
+const isOpen = ref(false);
+const isMobile = ref(false);
+</script>
+
 <template>
   <div class="m-10">
-    <router-link to="/" class="wrap-image text-xl m-10">home</router-link>
-    <router-link to="/people" class="wrap-image text-xl m-10"
-      >people</router-link
-    >
-    <router-link to="/starships" class="wrap-image text-xl m-10"
-      >starship</router-link
-    >
-    <router-link to="/vehicles" class="wrap-image text-xl m-10"
-      >vehicles</router-link
-    >
-    <router-link to="/species" class="wrap-image text-xl m-10"
-      >species</router-link
-    >
-    <router-link to="/planets" class="wrap-image text-xl m-10"
-      >planets</router-link
-    >
-    <router-link to="/films" class="wrap-image text-xl m-10">films</router-link>
-    <router-link to="/quizz" class="wrap-image text-xl m-10">quizz</router-link>
+    <div class="hidden sm:block">
+      <router-link to="/" class="wrap-image text-xl m-10">home</router-link>
+      <router-link to="/people" class="wrap-image text-xl m-10"
+        >people</router-link
+      >
+
+      <router-link to="/quizz" class="wrap-image text-xl m-10"
+        >quizz</router-link
+      >
+      <router-link to="/films" class="wrap-image text-xl m-10"
+        >films</router-link
+      >
+      <button
+        class="wrap-image text-xl m-10 relative"
+        @click="isOpen = !isOpen"
+      >
+        More
+      </button>
+      <div
+        v-show="isOpen"
+        class="flex flex-col w-72 bg-black bg-opacity-75 absolute top-26 right-12 z-10"
+      >
+        <router-link to="/vehicles" class="wrap-image text-xl m-10"
+          >vehicles</router-link
+        >
+        <router-link to="/species" class="wrap-image text-xl m-10"
+          >species</router-link
+        >
+        <router-link to="/planets" class="wrap-image text-xl m-10"
+          >planets</router-link
+        >
+        <router-link to="/starships" class="wrap-image text-xl m-10"
+          >starship</router-link
+        >
+      </div>
+    </div>
+
+    <div class="sm:hidden flex justify-between">
+      <nuxt-link to="/home" class="cursor-pointer">
+        <img
+          class="h-12 w-12"
+          src="https://upload.wikimedia.org/wikipedia/commons/5/5a/Star_Wars_Logo..png"
+        />
+      </nuxt-link>
+      <button class="relative" @click="isMobile = !isMobile">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 fill-yellow-300"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      </button>
+      <div
+        v-show="isMobile"
+        class="flex flex-col w-2/3 bg-black bg-opacity-75 absolute top-26 right-12 z-10"
+      >
+        <router-link to="/" class="wrap-image text-xl m-10">home</router-link>
+        <router-link to="/people" class="wrap-image text-xl m-10"
+          >people</router-link
+        >
+
+        <router-link to="/quizz" class="wrap-image text-xl m-10"
+          >quizz</router-link
+        >
+        <router-link to="/films" class="wrap-image text-xl m-10"
+          >films</router-link
+        >
+        <router-link to="/vehicles" class="wrap-image text-xl m-10"
+          >vehicles</router-link
+        >
+        <router-link to="/species" class="wrap-image text-xl m-10"
+          >species</router-link
+        >
+        <router-link to="/planets" class="wrap-image text-xl m-10"
+          >planets</router-link
+        >
+        <router-link to="/starships" class="wrap-image text-xl m-10"
+          >starship</router-link
+        >
+      </div>
+    </div>
   </div>
 </template>
 
