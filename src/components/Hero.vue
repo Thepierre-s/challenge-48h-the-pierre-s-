df5ff753bc109d777a81227c3b10d08e9d1f4dc1
bertyn99 <bboulikou@gmail.com>
2022-05-06 20:00:54 +0200

    fix padding pour grand écran

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,13 +1,18 @@
 <template>
-    <div class="flex flex-col justify-center items-center">
-       <h1 class="text-9xl font-bold tracking-normal m-20">star<br>wars</h1> 
-       <h2 class="text-5xl m-6">Hello young Jedi</h2>
-        <span class="text-xl">immerse yourself in the star wars universe and discover the secrets of the empire</span>
-    </div>
+  <div class="flex flex-col justify-center items-center p-10 md:py-28">
+    <h1 class="text-9xl font-bold tracking-normal m-8 md:m-12">
+      star<br />wars
+    </h1>
+    <h2 class="text-5xl m-6">Hello young Jedi</h2>
+    <span class="text-xl"
+      >immerse yourself in the star wars universe and discover the secrets of
+      the empire</span
+    >
+  </div>
 </template>
 
 <style scoped>
 h1 {
-    letter-spacing: 6px;
+  letter-spacing: 6px;
 }
 </style>
